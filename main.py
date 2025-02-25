8074eebb975e6c84a1b23c7d231123218aca0254
marcospsviana <marcospaulo.silvaviana@gmail.com>
2025-02-25 12:05:44 -0300

    update main window, add logo

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from tkinter import ttk
 from tkinter import Tk, PhotoImage
 
-class MainWindow(ttk.Widget):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        # self.create_widgets()
-        self.main_frame = ttk.Frame(self.master)
-        self.main_frame.pack()
-        self.master.title("Main Window")
-        self.master.geometry("800x600")
-        self.master.resizable(False, False)
-        self.master.configure(bg="white")
-        self.create_widgets()
-        self.logo = PhotoImage(file="logo.png")
-        self.master.iconphoto(False, self.logo)
-        self.logo.grid(row=0, column=0)
-        self.master.mainloop()
-
-if __name__ == "__main__":
-    root = Tk()
-    app = root.mainloop()
-    MainWindow(root)
-    
+root = Tk()
+root.title("My App")
+root.geometry("800x600")
+# root.iconphoto(False, PhotoImage(file="icon.png"))  # Set the icon of the window
+main = ttk.Frame(root, padding=(5, 5, 12, 12))
+main.grid(column=0, row=0)
+logo_img = PhotoImage(file="assets/gnupgEncrypter.png")
+logo = ttk.Label(main, image=logo_img)
+logo.grid(column=0, row=0, columnspan=2)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+root.mainloop()
