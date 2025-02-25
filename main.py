8a9ef839804b4640622fc887fcd8ee929ff237bb
marcospsviana <marcospaulo.silvaviana@gmail.com>
2025-02-25 11:48:13 -0300

    add logo and main frame

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from tkinter import ttk
+from tkinter import Tk, PhotoImage
 
 class MainWindow(ttk.Widget):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        # self.create_widgets()
+        self.main_frame = ttk.Frame(self.master)
+        self.main_frame.pack()
+        self.master.title("Main Window")
+        self.master.geometry("800x600")
+        self.master.resizable(False, False)
+        self.master.configure(bg="white")
         self.create_widgets()
-
+        self.logo = PhotoImage(file="logo.png")
+        self.master.iconphoto(False, self.logo)
+        self.logo.grid(row=0, column=0)
+        self.master.mainloop()
 
 if __name__ == "__main__":
-    root = ttk.Tk()
+    root = Tk()
     app = root.mainloop()
     MainWindow(root)
     
