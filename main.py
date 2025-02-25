de39b3d75087be4c2f5826cddeeb24ed1011f206
marcospsviana <marcospaulo.silvaviana@gmail.com>
2025-02-25 11:42:50 -0300

    initial main window

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from gnupg-key-generator!")
+from tkinter import ttk
+
+class MainWindow(ttk.Widget):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
 
 
 if __name__ == "__main__":
-    main()
+    root = ttk.Tk()
+    app = root.mainloop()
+    MainWindow(root)
+    
