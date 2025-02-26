19ac412ade84e3c6f1327aa1e7a2a8896eedc862
marcospsviana <marcospaulo.silvaviana@gmail.com>
2025-02-26 20:11:55 -0300

    test pass real name and name gnupghome

--- main.py
+++ main.py
@@ -8,16 +8,44 @@ import os
 import time
 
 def get_name_gnupghome(entity):
-    return f"{entity} {time.strftime('%Y')}-{time.strftime('%H_%M_%S')}"
+    return f"{entity} {time.strftime('%Y-%m-%d')}"
 
 
-def generate_key_pair(type, size, alg):
-    ...
+def make_directories_name(entity):
+    dest_current_key = ""
+    try:
+        os.makedirs(f"{os.path.expanduser("~")}/.gnugenerator", exist_ok=False)
+    except FileExistsError:
+        ...
+    gpnupghome = get_name_gnupghome(entity)
+    formated_name = "".join(gpnupghome.split())
+    dest_current_key = f"{os.path.expanduser("~")}/.gnugenerator/{formated_name}"
+    os.makedirs(dest_current_key, exist_ok=True)
+    return dest_current_key
+
+
+def generate_key_pair(type, size, alg, email, entity):
+    
+    dest_current_key = make_directories_name(entity=entity)
+    real_name = get_name_gnupghome(entity=entity)
+    gpg = gnupg.GPG(gnupghome=dest_current_key)
+    gpg.encoding = 'utf-8'
+    key_input = gpg.gen_key_input(
+        name_real=real_name,
+        name_email=f"{email}",
+        key_type=type,
+        key_length=size,
+        subkey_type=type,
+        subkey_length=size,
+        expire_date=365,
+        passphrase='passphrase'
+    )
+    key = gpg.gen_key(key_input)
+    return key
 
 root = Tk()
 root.title("My App")
 root.geometry("800x600")
-# root.iconphoto(False, PhotoImage(file="icon.png"))  # Set the icon of the window
 main = ttk.Frame(root, padding=(5, 5, 12, 12))
 main.grid(column=0, row=0)
 logo_img = PhotoImage(file="assets/gnupgEncrypter.png")
